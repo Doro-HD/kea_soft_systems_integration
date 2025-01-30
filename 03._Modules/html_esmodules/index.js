dd99a0301c5992e40f9dd3a19724c71661314729
doro-hd <doro-hd@protonmail.com>
2025-01-30 12:31:04 +0100

    Class 30/01/25 finished

---
+++ 03._Modules/html_esmodules/index.js
@@ -0,0 +1,4 @@
+import ClassA from './myModule';
+
+const classA = new ClassA();
+
---
+++ 03._Modules/html_esmodules/myModule.js
@@ -0,0 +1,8 @@
+class ClassA {
+	constructor() {
+		console.log('CLass A initialized');
+	}
+}
+
+export default ClassA;
+
